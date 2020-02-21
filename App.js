f52d37f570df374e498a0656947f39726d033c64
carloscampos15 <carlos2954@hotmail.com>
2020-02-21 16:08:36 -0500

    install drawer navigation

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, StyleSheet } from 'react-native';
+import DrawerNavigation from './navigation/DrawerNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <DrawerNavigation/>
   );
 }
 
