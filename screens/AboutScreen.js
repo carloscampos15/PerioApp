53db252949299fc5931e754c961f4dd4dc3afb2a
carloscampos15 <carlos2954@hotmail.com>
2020-02-22 22:35:28 -0500

    add drawer navigatiobn

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,33 @@
 import * as React from 'react';
 import { Button, View,Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function AboutScreen({ navigation }) {
+function About(){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>SCREEN ABOUT</Text>
     </View>
+  );  
+}
+
+const Stack = createStackNavigator();
+
+export default function AboutScreen({ navigation }) {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="About" 
+          component={About}
+          options={{
+            title: 'About',
+            headerStyle: {
+              backgroundColor: '#0069a3',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}/>
+      </Stack.Navigator>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,50 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function HomeScreen({ navigation }) {
+function Home(){
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>SCREEN HOME</Text>
     </View>
+  );  
+}
+
+const Stack = createStackNavigator();
+
+export default function HomeScreen({ navigation }) {
+  return (
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home" 
+          component={Home} 
+          options={{
+            title: 'Home',
+            // headerLeft: () => (
+            //   <TouchableOpacity 
+            //     activeOpacity={0.5}>
+            //     <Image
+            //       source={require('./../assets/images/menu.png')}
+            //       style= {styles.MenuStyle}
+            //     />
+            //   </TouchableOpacity>
+            // ),
+            headerStyle: {
+              backgroundColor: '#0069a3',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}/>
+      </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  MenuStyle: {
+    width: 33,
+    height: 33,
+    margin: 20,
+  },
+});
